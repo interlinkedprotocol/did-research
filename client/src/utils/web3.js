3eed8e98f8baefb3abb881f4d96bdbdfe9c0de51
Andrey Orlov <aaorlov@Andreys-MacBook-Pro.local>
2018-09-27 12:02:24 +0300

    fix

--- client/src/utils/web3.js
+++ client/src/utils/web3.js
@@ -1,7 +1,7 @@
 import Web3  from  'web3'
 import HttpProvider from 'ethjs-provider-http'
 
-const blockchainURL = `http://${process.env.REACT_APP_BLOCKCHAIN_PORT}:${process.env.REACT_APP_BLOCKCHAIN_PORT}`
+const blockchainURL = `http://${process.env.REACT_APP_NETWORK_HOST}:${process.env.REACT_APP_BLOCKCHAIN_PORT}`
+
 const provider = new HttpProvider(blockchainURL)
 export const web3Instance = new Web3(provider)
-
