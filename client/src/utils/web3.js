a9e04124f8e83335b69107497d24c69d740caec2
Andrey Orlov <aaorlov@Andreys-MacBook-Pro.local>
2018-09-28 10:27:35 +0300

    infura

--- client/src/utils/web3.js
+++ client/src/utils/web3.js
@@ -1,7 +1,8 @@
 import Web3  from  'web3'
 import HttpProvider from 'ethjs-provider-http'
 
-const blockchainURL = `http://${process.env.REACT_APP_NETWORK_HOST}:${process.env.REACT_APP_BLOCKCHAIN_PORT}`
+// const blockchainURL = `http://${process.env.REACT_APP_NETWORK_HOST}:${process.env.REACT_APP_BLOCKCHAIN_PORT}`
+const blockchainURL = `https://ropsten.infura.io/ethr-did'`
 console.log(blockchainURL);
 const provider = new HttpProvider(blockchainURL)
 export const web3Instance = new Web3(provider)
